d27bb3e672c09f9175d0f79a9dec3f1808e1693c
Mia Pelino <maria.pelino@gmail.com>
2017-11-05 19:25:03 -0500

    Update Program.cs

--- Capstone/Program.cs
+++ Capstone/Program.cs
@@ -1,16 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Capstone.Classes;
 
 namespace Capstone
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
 
+            FileIO fileIO = new FileIO();
+
+
+            VendingMachine ourVendMach = new VendingMachine(fileIO.CreateInventoryList());
+
+            CommandLineInterface ourCLI = new CommandLineInterface(ourVendMach);
+
+            ourCLI.Runner();
         }
     }
 }
